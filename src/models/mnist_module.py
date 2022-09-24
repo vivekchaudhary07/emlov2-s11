f1507eddde448816548f60918468c143f682506c
Satyajit Ghana <satyajitghana7@gmail.com>
2022-09-24 06:33:05 +0000

    add demo code

--- src/models/mnist_module.py
+++ src/models/mnist_module.py
@@ -4,6 +4,8 @@ import torch
 from pytorch_lightning import LightningModule
 from torchmetrics import MaxMetric, MeanMetric
 from torchmetrics.classification.accuracy import Accuracy
+import torch.nn.functional as F
+from torchvision import transforms as T
 
 
 class MNISTLitModule(LightningModule):
@@ -32,6 +34,8 @@ class MNISTLitModule(LightningModule):
         # also ensures init params will be stored in ckpt
         self.save_hyperparameters(logger=False, ignore=["net"])
 
+        # add tensorboard hp_metric logging here
+
         self.net = net
 
         # loss function
@@ -50,8 +54,23 @@ class MNISTLitModule(LightningModule):
         # for tracking best so far validation accuracy
         self.val_acc_best = MaxMetric()
 
+        self.predict_transform = T.Normalize((0.1307,), (0.3081,))
+
     def forward(self, x: torch.Tensor):
         return self.net(x)
+
+    @torch.jit.export
+    def forward_jit(self, x: torch.Tensor):
+        with torch.no_grad():
+            # transform the inputs
+            x = self.predict_transform(x)
+
+            # forward pass
+            logits = self(x)
+
+            preds = F.softmax(logits, dim=-1)
+
+        return preds
 
     def on_train_start(self):
         # by default lightning executes validation step sanity checks before training starts,
